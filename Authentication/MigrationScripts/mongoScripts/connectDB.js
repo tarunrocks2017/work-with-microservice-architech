92110b8ce7e247371cc9a1d290f75e58ef52ea7d
tarun kumar <tarunharsh2017@gmail.com>
2019-01-17 18:57:08 +0530

    modification in migration

--- Authentication/MigrationScripts/mongoScripts/connectDB.js
+++ Authentication/MigrationScripts/mongoScripts/connectDB.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const config = require('../../config/urlConfig');
 
 async function getMongoConnection() {
-  const dbName = process.env.MONGO_DB || config.DB_NAME;
-  mongoose.connect(`${config.MONGO_URL}${dbName}`, {
+  // const dbName = process.env.MONGO_DB || config.DB_NAME;
+  mongoose.connect(`${config.MONGO_URL}`, {
     useNewUrlParser: true,
     useCreateIndex: true,
   });
--- Authentication/config/urlConfig.js
+++ Authentication/config/urlConfig.js
@@ -1,10 +1,9 @@
 const AUTH_URL = 'http://localhost:4002';
 let MONGO_URL = 'mongodb://localhost:27017/';
-let DB_NAME = 'tarunMongoDB';
+const DB_NAME = 'tarunMongoDB';
 
 if (process.env.MODE === 'prod') {
-  MONGO_URL = 'mongo ds159204.mlab.com:59204';
-  DB_NAME = 'tarunmongodb';
+  MONGO_URL = 'mongodb://tarun:tarunharsh2019@ds159204.mlab.com:59204/tarunmongodb';
 }
 
 module.exports = {
