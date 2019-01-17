76ed05bcbc5170897669985ce38e2f8fa01b5167
tarun kumar <tarunharsh2017@gmail.com>
2019-01-17 19:17:48 +0530

    middlware modification Library app

--- LibraryApp/middlewareFunctions/middleWare.js
+++ LibraryApp/middlewareFunctions/middleWare.js
@@ -1,7 +1,6 @@
 const winston = require('../config/winston');
 
-// const connection = require('../MigrationScripts/mongoScripts/connectDB');
-const collection = require('../User/userModel');
+// const collection = require('../User/userModel');
 
 const logger = (req, res, next) => {
   winston.log('info', `${req.url} requested`);
@@ -23,19 +22,18 @@ const pageRedirectingToLogin = (req, res, next) => {
   }
 };
 
-const checkUser = async (req, res, next) => {
-  // connection.getMongoConnection();
-  const userExist = await collection.User.find({ email: req.body.email });
-  if (userExist.length > 0) {
-    res.status(422).render('pages/registration', { b: true, errors: ['user already exist'], isActive: false });
-  } else {
-    next();
-  }
-};
+// const checkUser = async (req, res, next) => {
+//   // connection.getMongoConnection();
+//   const userExist = await collection.User.find({ email: req.body.email });
+//   if (userExist.length > 0) {
+//     res.status(422).render('pages/registration', { b: true, errors: ['user already exist'], isActive: false });
+//   } else {
+//     next();
+//   }
+// };
 
 module.exports = {
   logger,
   RedirectLogin,
   pageRedirectingToLogin,
-  checkUser,
 };
