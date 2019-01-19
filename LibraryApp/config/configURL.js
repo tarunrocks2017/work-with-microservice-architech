3b4a1677e610dc1d391171099216ece057bd7821
tarun kumar <tarunharsh2017@gmail.com>
2019-01-19 10:17:33 +0530

    ecosystem file commit

--- LibraryApp/config/configURL.js
+++ LibraryApp/config/configURL.js
@@ -2,7 +2,7 @@ let MYSQL_ACTOR_URL = 'http://localhost:8080/actors';
 let MYSQL_MOVIE_URL = 'http://localhost:8080/movies';
 const TEST_URL = 'http://localhost:4001';
 
-if (process.env.MODE === 'prod') {
+if (process.env.NODE_ENV === 'production') {
   MYSQL_ACTOR_URL = `http://${process.env.IP}/actors`;
   MYSQL_MOVIE_URL = `http://${process.env.IP}/movies`;
 }
